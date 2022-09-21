120d28118e0eca46ea6592361120e084d1961cc7
Mads Mikkel Rasmussen <mara@aspit.dk>
2022-09-21 09:32:26 +0200

    Add project files.

---
+++ Entities/Basket.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Basket
+    {
+        private List<Product> products;
+
+        public Basket()
+        {
+            products = new List<Product>();
+        }
+
+        public Basket(List<Product> products)
+        {
+            Products = products;
+        }
+
+        public List<Product> Products
+        {
+            get => products;
+            private set
+            {
+                if (value is null)
+                    throw new ArgumentNullException(nameof(value));
+                products = value;
+            }
+        }
+
+        public void Add(Product product)
+        {
+            if (product is null)
+            {
+                throw new ArgumentNullException(nameof(product));
+            }
+
+            products.Add(product);
+        }
+
+        public void Remove(Product product)
+        {
+            if (product is null)
+            {
+                throw new ArgumentNullException(nameof(product));
+            }
+
+            // TODO: consider using the bool return value from the List.Remove method.
+            products.Remove(product);
+        }
+    }
+}
---
+++ Entities/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Customer
+    {
+        private Basket basket;
+        private DateTime creationDate;
+
+        public Customer(Basket basket)
+        {
+            Basket = basket;
+        }
+
+        public Basket Basket
+        {
+            get => basket;
+            set
+            {
+                if (value is null)
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+                basket = value;
+            }
+        }
+
+        public DateTime CreationDate { get => creationDate; set => creationDate = value; }
+    }
+}
